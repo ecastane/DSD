1ac07e8fdb68954516dd361d322948f43484b76a
ecastane <ecastane@stevens.edu>
2020-09-25 20:12:59 -0400

    Chapter 7 Exercise 1

---
+++ exercise1_dff.vhdl
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity d_ff is
+	port (	D, R, S	:	in  std_logic;
+				CLK	:	in  std_logic;	
+				Q	:	out std_logic);
+end d_ff;
+
+architecture my_d_ff of d_ff is
+begin
+	dff:	process (R, S, CLK)
+	begin
+		if (S = '0') then 
+			Q <= '1';
+		elsif (R = '0') then
+			Q <= '0'
+		elsif (falling_edge(CLK)) then
+			Q <= D;
+		end if;
+	end process dff;
+end my_d_ff;
